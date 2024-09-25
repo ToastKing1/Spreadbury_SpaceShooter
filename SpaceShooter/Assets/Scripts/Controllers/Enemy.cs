bdd68a151934769a39ae4579438e915882b1b433
ToastKing1 <owenspreadbury1@gmail.com>
2024-09-24 23:08:21 -0400

    enemy movement code

--- SpaceShooter/Assets/Scripts/Controllers/Enemy.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Enemy.cs
@@ -17,11 +17,14 @@ public class Enemy : MonoBehaviour
     }
     private void Update()
     {
-        float distance = Mathf.Sqrt((playerTransform.position.x - transform.position.x) + (playerTransform.position.y - transform.position.y));
+        float distance = Mathf.Sqrt((playerTransform.position.x - transform.position.x)* (playerTransform.position.x - transform.position.x) + 
+            
+            (playerTransform.position.y - transform.position.y)* (playerTransform.position.y - transform.position.y));
+
 
         Debug.Log(distance);
 
-        if (distance < 2f)
+        if (distance < 3.5f)
         {
             velocity = Vector3.zero;
             Debug.DrawLine(transform.position, playerTransform.position, Color.red);
@@ -30,6 +33,11 @@ public class Enemy : MonoBehaviour
         {
 
             velocity += (playerTransform.position - transform.position) * acceleration * Time.deltaTime;
+
+            if (velocity.magnitude > targetSpeed)
+            {
+                velocity = velocity.normalized * targetSpeed;
+            }
 
 
             transform.position += velocity * Time.deltaTime;
