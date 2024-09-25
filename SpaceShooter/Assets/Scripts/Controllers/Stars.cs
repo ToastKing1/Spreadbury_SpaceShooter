2d2de1833d4477fa2bbe9be66b432b7a6be60468
ToastKing1 <owenspreadbury1@gmail.com>
2024-09-25 12:26:33 -0400

    finished stars script

--- SpaceShooter/Assets/Scripts/Controllers/Stars.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Stars.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+
+    public int star = 0;
 
     // Update is called once per frame
     void Update()
@@ -16,13 +19,21 @@ public class Stars : MonoBehaviour
     void DrawConstellation()
     {
 
-        for (int i = 0; i < starTransforms.Count - 1; i++)
-        {
+        drawingTime += 1 * Time.deltaTime;
 
-                Debug.DrawLine(starTransforms[i].position, starTransforms[i + 1].position, Color.white);
-            
-        
+        Vector3 endPoint = Vector3.Lerp(starTransforms[star].position, starTransforms[(star + 1) % starTransforms.Count].position, drawingTime);
+
+                Debug.DrawLine(starTransforms[star].position, endPoint, Color.white);
+
+        if (drawingTime > 1)
+        {
+            drawingTime = 0;
+            star = (star + 1) % starTransforms.Count;
         }
+            
+            
+
+        
 
 
 
