a242f2f4544c243688aa0c98350cd21cd29f3062
ToastKing1 <owenspreadbury1@gmail.com>
2024-09-30 17:18:16 -0400

    task 3 done

--- SpaceShooter/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Moon.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+
+    public float radius = 2f;
+    public float speed = 1f;
+    float angleAmount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +20,13 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(radius, speed, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        angleAmount += speed*Time.deltaTime;
+
+        transform.position = planetTransform.position + new Vector3(Mathf.Cos(angleAmount), Mathf.Sin(angleAmount), 0f) * radius;
     }
 }
