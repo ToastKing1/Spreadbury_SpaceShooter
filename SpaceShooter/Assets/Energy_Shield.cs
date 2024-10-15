fabe8a0de744a04cf7bea11a1eb844e4bcc7ff2b
ToastKing1 <owenspreadbury1@gmail.com>
2024-10-15 09:51:35 -0400

    energy shield motion done

---
+++ SpaceShooter/Assets/Energy_Shield.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Energy_Shield : MonoBehaviour
+{
+
+    public float radius = 2;
+    public float speed = 2;
+    public float time = 0;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Motion();
+
+        Rotation();
+    }
+
+    public void Motion()
+    {
+        if (time > 14.15)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            time += speed * Time.deltaTime;
+
+            transform.position = player.position + new Vector3(Mathf.Cos(time), Mathf.Sin(time), 0f) * radius;
+        }
+    }
+
+    public void Rotation()
+    {
+
+    }
+}
